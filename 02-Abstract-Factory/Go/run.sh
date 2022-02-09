72d61236664bce365ae1407ba10237f390430858
azhong <zczwzw@163.com>
2022-02-09 21:26:49 +0800

    abstract factory go

--- 02-Abstract-Factory/Go/run.sh
+++ 02-Abstract-Factory/Go/run.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/bash
-go run adidas.go adidasShoe.go  iShoe.go  main.go  nikeShirt.go adidasShirt.go iShirt.go  iSportsFactory.go nike.go nikeShoe.go
+# go run adidas.go adidasShoe.go iShoe.go main.go nikeShirt.go adidasShirt.go iShirt.go iSportsFactory.go nike.go nikeShoe.go
+
+cmd="go run "
+for i in *.go
+do
+    cmd+=$i" "
+done
+$cmd
+
+
